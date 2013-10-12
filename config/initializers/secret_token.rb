8fac94a02ac970971c06bbab0840198fe8d721a8
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-10-12 21:23:58 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myheroku::Application.config.secret_key_base = '3e681c336178a125509ea3c44d9d3705a4e1cfd090d21f3d2c5fc749a106a4b95d07092d89520252cf0111e27c92343d665b7bd9204e495e1fdf0000a85ecee5'
