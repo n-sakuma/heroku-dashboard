c7a26dc4ab7e810d1365e42bb22f8771301067d2
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-10-12 22:18:32 +0900

    Add dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Myheroku::Application.routes.draw do
+
+  root 'dashbord#index'
+
+  get "dashbord/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +43,7 @@ Myheroku::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
