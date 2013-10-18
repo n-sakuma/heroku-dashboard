e944c702fd7404bf6578919944551058971691f3
Naohiro Sakuma <nao.bear@gmail.com>
2013-10-18 15:29:57 +0900

    refactor: extract method

--- app/models/app_group.rb
+++ app/models/app_group.rb
@@ -1,4 +1,12 @@
 class AppGroup < ActiveRecord::Base
   self.table_name = 'tags'
+
+  def self.grouping
+    grouping = {}
+    pluck(:name).each do |k|
+      grouping[k.to_sym] = HerokuApp.tagged_with(k).map{|a| Api::App.new(a.name)}
+    end
+    grouping
+  end
 end
 
