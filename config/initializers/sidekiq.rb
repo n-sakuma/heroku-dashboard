71b0a18db4788cd4cbe4e05df33a27e089df4ab8
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-11-21 13:29:02 +0900

    change sidekiq config (redis url)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,8 @@
 Sidekiq.configure_server do |config|
   config.poll_interval = 15
 
+  config.redis = { url: (ENV['REDISCLOUD_URL'] || 'redis://localhost:6379') }
+
   # for Heroku
   database_url = ENV['DATABASE_URL']
   if database_url
