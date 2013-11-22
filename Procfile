e2f097f57987263df71394455fb3753bc31980d3
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-11-22 11:58:02 +0900

    change setting for sidekiq

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: bundle exec rails server -p $PORT
-worker: bundle exec sidekiq
+worker: bundle exec sidekiq -e production -C config/sidekiq.yml
 
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,17 @@
-Sidekiq.configure_server do |config|
-  config.poll_interval = 15
-
+heroku_setting = proc do |config|
   config.redis = { url: (ENV['REDISCLOUD_URL'] || 'redis://localhost:6379') }
 
-  # for Heroku
-  database_url = ENV['DATABASE_URL']
-  if database_url
+  if database_url = ENV['DATABASE_URL']
     ENV['DATABASE_URL'] = "#{database_url}?pool=25"
     ActiveRecord::Base.establish_connection
   end
+end
 
+Sidekiq.configure_server do |config|
+  config.poll_interval = 15
+  heroku_setting.call(config)
+end
+
+Sidekiq.configure_client do |config|
+  heroku_setting.call(config)
 end
